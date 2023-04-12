7c5f75cba3328e01f0b0578f568bff2a68ac5281
Rose Wachira <laurose20@gmail.com>
2023-04-11 22:45:13 -0500

    commiting changes to printJobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -59,12 +59,12 @@ public class TechJobs {
 
                 // What is their search term?
                 System.out.println("\nSearch term:");
-                String searchTerm = in.nextLine();
+                String searchTerm = in.nextLine().toLowerCase();
 
                 if (searchField.equals("all")) {
-                    printJobs(JobData.findByValue(searchTerm));
+                    printJobs(JobData.findByValue(searchTerm.toLowerCase()));
                 } else {
-                    printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
+                    printJobs(JobData.findByColumnAndValue(searchField.toLowerCase(), searchTerm.toLowerCase()));
                 }
             }
         }
@@ -119,7 +119,41 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        // loop through the array
+        if(someJobs.size() == 0){
+            System.out.println("No Results");
 
-        System.out.println("printJobs is not implemented yet");
+        }else{
+            for(HashMap<String, String> job: someJobs){
+                System.out.println("*****");
+                for(Map.Entry<String, String> categories: job.entrySet()){
+                    System.out.println(categories.getKey() + ": " + categories.getValue());
+                }
+                System.out.println("******\n");
+            }
+        }
+//            for(HashMap<String,String> job: someJobs){
+//                System.out.println("*****");
+//                for(String key: job.keySet()){
+//                    System.out.println(key + ":" + job.get(key));
+//                }
+//                System.out.println("*****\n");
+//            }
+//        }
+//        else{
+//            System.out.println("There are no jobs found.");
+//        }
+//            System.out.println("No jobs");
+//        else{
+//            for(int num=0; num< someJobs.size(); num++){
+//                System.out.println("********");
+//                for (Map.Entry<String,String> categories: someJobs.get(num).entrySet()){
+//                    System.out.println(categories.getKey() + ":" + categories.getValue());
+//                }
+//                System.out.println("********\n");
+//            }
+//        }
+
+        //System.out.println("printJobs is not implemented yet");
     }
 }
